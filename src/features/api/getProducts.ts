d7fc0c38b69be822020912242b6ec43b07ed5c6e
SebellKo <79312827+SebellKo@users.noreply.github.com>
2025-05-14 00:50:38 +0900

    [feat]: getProduct api 및 res type 구현

---
+++ src/features/api/getProducts.ts
@@ -0,0 +1,21 @@
+import { ProductRes } from '@/features/types/api';
+
+interface Props {
+  limit: number;
+  skip: number;
+}
+
+export const getProducts = async ({
+  limit,
+  skip,
+}: Props): Promise<ProductRes> => {
+  try {
+    const response = await fetch(
+      `https://dummyjson.com/products?limit=${limit}&skip=${skip}`
+    );
+    return response.json();
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
