89e8936b1068b9f954aba28ae65aa58a25ba06b4
SebellKo <79312827+SebellKo@users.noreply.github.com>
2025-05-14 23:22:23 +0900

    [feat]: SearchPage 구현

--- src/app/(default)/search/page.tsx
+++ src/app/(default)/search/page.tsx
@@ -1,5 +1,13 @@
-import List from '@/features/Home/components/List/List';
+import SearchList from '@/features/Search/components/SearchList';
+import { getSearch } from '@/features/Search/api/getSearch';
 
-export default async function Search() {
-  return <List />;
+export default async function Search({
+  searchParams,
+}: {
+  searchParams: Promise<{ query: string }>;
+}) {
+  const { query } = (await searchParams) || '';
+  const initialData = await getSearch(query);
+
+  return <SearchList initialData={initialData} query={query} />;
 }
