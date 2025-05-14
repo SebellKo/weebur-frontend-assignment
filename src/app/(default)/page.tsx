77af4a259ef417eedb722d7eed0f54b0515f0fbf
SebellKo <79312827+SebellKo@users.noreply.github.com>
2025-05-14 23:23:14 +0900

    [feat]: HomePage 별도 분리

--- src/app/(default)/page.tsx
+++ src/app/(default)/page.tsx
@@ -1,8 +1,8 @@
-import List from '@/features/Home/components/List/List';
 import { getProducts } from '@/features/Home/api/getProducts';
+import HomeList from '@/features/Home/components/HomeList';
 
 export default async function Home() {
   const initialData = await getProducts({ limit: 20, skip: 0 });
 
-  return <List initialData={initialData} />;
+  return <HomeList initialData={initialData} />;
 }
